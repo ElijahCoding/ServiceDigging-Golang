f494299fb28e30ba71de20b2cbbe1233ad736e1d
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-27 21:02:56 +0300

    gentally shutdown

--- 02-shutdown/main.go
+++ 02-shutdown/main.go
@@ -1,5 +1,76 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func main() {
+	log.Printf("main: Started")
+	defer log.Printf("main:  Completed")
+
+	api := http.Server{
+		Addr:         "localhost:8080",
+		Handler:      http.HandlerFunc(Echo),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("main : API listening on %s", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("error: listening and serving: %s", err)
+	case <-shutdown:
+		log.Println("main : Start shutdown")
+
+		// Give outstanding requests a deadline for completion.
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		// Asking listener to shutdown and load shed.
+		err := api.Shutdown(ctx)
+		if err != nil {
+			log.Printf("main : Graceful shutdown did not complete in %v : %v", timeout, err)
+			err = api.Close()
+		}
+
+		if err != nil {
+			log.Fatalf("main : could not stop server gracefully : %v", err)
+		}
+	}
+
+	h := http.HandlerFunc(Echo)
+	log.Println("Listening on localhost:8080")
+	if err := http.ListenAndServe("localhost:8080", h); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Echo(w http.ResponseWriter, r *http.Request) {
+	n := rand.Intn(1000)
+	log.Println("start", n)
+	defer log.Println("end", n)
+	time.Sleep(3 * time.Second)
+	fmt.Fprintln(w, "You asked to ", r.Method, r.URL.Path)
 }
