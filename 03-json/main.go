3aaba89e92bf2ba78e69666cb516e73c19049ab1
ElijahCoding <cuiyeqing960904@gmail.com>
2020-08-03 11:12:16 +0300

    listening shutdown error

--- 03-json/main.go
+++ 03-json/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	log.Printf("main: Started")
+	defer log.Println("main: Completed")
+
+	api := http.Server{
+		Addr: "localhost:8080",
+		Handler: http.HandlerFunc(ListProducts),
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
+	serverErrors := make(chan error, 1)
+
+	// Start the service listening for requests.
+	go func() {
+		log.Printf("main : API listening on %s", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+
+}
