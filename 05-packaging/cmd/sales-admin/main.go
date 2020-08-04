8c71de5c26890f2cd4e5aca7e7683eb8529408f1
ElijahCoding <cuiyeqing960904@gmail.com>
2020-08-04 19:23:34 +0300

    refactored into sales-admin

--- 05-packaging/cmd/sales-admin/main.go
+++ 05-packaging/cmd/sales-admin/main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"flag"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"log"
+	"net/url"
+	schema "service-template/05-packaging/internal/shcema"
+)
+
+func main() {
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	flag.Parse()
+	switch flag.Arg(0) {
+	case "migrate":
+		if err := schema.Migrate(db); err != nil {
+			log.Fatal("applying migrations", err)
+		}
+		log.Println("Migrations complete")
+		return
+	case "seed":
+		if err := schema.Seed(db); err != nil {
+			log.Fatal("applying seed data", err)
+		}
+		log.Println("Seeds complete")
+		return
+	}
+}
+
+func openDB() (*sqlx.DB, error) {
+	q := url.Values{}
+	q.Set("sslmode", "disable")
+	q.Set("timezone", "utc")
+
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword("postgres", "root"),
+		Host:     "localhost:54321",
+		Path:     "postgres",
+		RawQuery: q.Encode(),
+	}
+
+	return sqlx.Open("postgres", u.String())
+}
